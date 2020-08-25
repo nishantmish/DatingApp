62be5723140294a75f1bbe57f3604e3f13bf2527
nishantmish <nishant.7895.nm@gmail.com>
2020-08-25 12:12:09 +0530

    message

---
+++ DatingApp.API/Migrations/20200824045411_InitialCreateXUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class InitialCreateXUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Values",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Values",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
